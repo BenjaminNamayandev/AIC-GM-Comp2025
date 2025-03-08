967cfd7da03554d49b58455dee799f0963bc17a6
benjaminnamayandev <benjaminnamayandeh@gmail.com>
2025-03-08 11:49:15 -0500

    Updated this bih

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
 
+# Load a pretrained model (for example, YOLOv11 medium)
 model = YOLO("yolo11m.pt")
 
+# Train the model
 model.train(data="config.yaml", 
-            imgsz = 640, batch = 8, 
-            epochs = 100, workers = 1, 
-            cache = False,
-            device = "cpu")
-
+            imgsz=640, 
+            batch=8, 
+            epochs=100, 
+            workers=1,
+            cache=False,
+            device="cpu")
