080f918620cc5e2778c7ade8dfcb6427288a5988
redmac135 <ethanzhao1981@gmail.com>
2025-03-08 23:20:11 -0500

    add n and s models

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import torch
-print(torch.cuda.is_available())
-print(torch.version.cuda)
+from ultralytics import YOLO
+
+model = YOLO("models/best-11n.pt")
+
+results = model("ethan/data/test-thermal-data/test_images_8_bit/image_1.jpeg")
+
+for result in results:
+    boxes_np = result.boxes.xyxy.cpu().numpy()
+    for box, box_np in zip(result.boxes, boxes_np):
+        print(f"Class: {int(box.cls)}, Confidence: {float(box.conf)}, Bounding Box: {box_np}")
